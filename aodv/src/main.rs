e2d5cb0e3598b4d9d827e117d3425cae5690a568
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-05-20 08:45:59 +0200

    added a test struct to the project

--- aodv/src/main.rs
+++ aodv/src/main.rs
@@ -1,3 +1,7 @@
+mod simulator;
+
+use simulator::Simulator;
+
 use actix_web::{get, post, web::Json, App, HttpServer};
 use actix_cors::Cors;
 
@@ -15,5 +19,6 @@ async fn main() -> std::io::Result<()> {
 
 #[get("/test_connection")]
 async fn test_connection()  -> String {
-    String::from("Connected")
+    let sim = Simulator::new(3);
+    format!("Connected, {}", sim.drones)
 }
---
+++ aodv/src/simulator.rs
@@ -0,0 +1,11 @@
+pub struct Simulator {
+    pub drones: u32
+}
+
+impl Simulator {
+    pub fn new(drones:u32) -> Self {
+        Self {
+            drones
+        }
+    }
+}
